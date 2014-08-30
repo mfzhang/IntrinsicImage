7f6b84cedf89d5fdfba55cfb77b820949b18998b
soundsilence <soundsilencebisai@gmail.com>
2014-08-30 20:28:18 +0800

    finish: main function

--- src/main.cc
+++ src/main.cc
@@ -14,19 +14,48 @@ int main(){
     double sigma = 0; 
     int k = 30;
     int min_size = 1;
+    
+    // read image 
     CVImage original_image = imread(original_image_path);
-    Mat_<uchar> mask_image = imread(mask_path,0);
+    Mat_<uchar> original_image_gray = imread(original_image_path,0);  
+    int image_width = original_image.cols;
+    int image_height = original_image.rows; 
     image<rgb> input = MatToImage(original_image); 
     imshow("Original image", original_image); 
+    
+    // segment the image into superpixels and get clusters 
     int num_css; 
     printf("Segment image...\n");
-    image<rgb> *output = segment_image(&input, sigma, k, min_size, &num_css);
-    CVImage segment_result = ImageToMat(*output);        
-    imshow("Segment result", segment_result);
-    printf("got %d components\n", num_css);
-    printf("done! uff...thats hard work.\n");
+    vector<ReflectanceCluster> clusters = GetReflectanceCluster(&input, sigma, k, min_size, &num_css);
     waitKey(0); 
+    
+    // Get pairwise weight of clusters
+    Mat_<double> pairwise_weight = GetPairwiseWeight(clusters,original_image);
+
+    // Initial reflectance to log(intensity) 
+    Mat_<double> log_image(image_height,image_width,0);
+    for(int i = 0;i < image_height;i++){
+        for(int j = 0;j < image_width.cols;j++){
+            // prevent log(0)
+            log_image(i,j) = log(original_image_gray(i,j) + 1); 
+        }
+    }
+     
+    // Solve reflectance
+    int alpha = ;
+    int mu = ;
+    int iteration_num = ;
+    int lambda = ;
+    Mat_<double> reflectance = log_image.clone();
+    reflectance = L1Regularization(pairwise_weight, reflectance, intensity, alpha, mu, lambda, iteration_num);
+    
+    // Solve shading
+    Mat_<double> ratio = ShadingSmooth(reflectance, log_image);
+    
+
     return 0;
+
+
 }
 
 
